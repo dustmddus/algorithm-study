e67d76c1c121221a3643005544d2f6eabbc6d4f2
Seung Yeon <82739503+dustmddus@users.noreply.github.com>
2022-05-24 21:31:22 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,20 @@
+'''
+인접한 모든 자리의 차이가 1이다. 이런 수를 계단수라고 한다.
+n이 주어질 때, 길이가 n인 계단수가 총 몇개인지 구해보자.
+
+'''
+n=int(input())
+dp=[[0]*10 for _ in range(n+1)]
+for i in range(1,10):
+    dp[1][i]=1
+MOD = 1000000000
+
+for i in range(2,n+1):
+    for j in range(10):
+        if j==0:
+            dp[i][j]=dp[i-1][1]
+        elif j==9:
+            dp[i][j]=dp[i-1][8]
+        else:
+            dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1]
+print(sum(dp[n])%MOD)
