0223b2732a33e846216c3642640840213e84ac3c
Seung Yeon <82739503+dustmddus@users.noreply.github.com>
2022-05-26 10:06:10 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 116 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,13 +1,6 @@
-'''
-수열 A가 주어졌을 떄, 가장 긴 증가하는 부분수
-1. 테이블 정하기
-d[i]=i까지 증가하는 가장 큰 수
-2.. 점화식
-'''
 n=int(input())
-d=[1 for _ in range(n)]
 arr=list(map(int,input().split()))
-
+d=[1 for _ in range(n)]
 for i in range(n):
     for j in range(i):
         if arr[i]>arr[j]:
@@ -21,3 +14,6 @@ for i in range(n-1,-1,-1):
         order-=1
 result.reverse()
 print(*result)
+
+    
+
