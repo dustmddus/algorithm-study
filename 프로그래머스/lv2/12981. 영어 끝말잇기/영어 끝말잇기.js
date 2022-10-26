d2e2f5d338bf9ce2f77489839c370e4dcb5b2a14
Seung Yeon <82739503+dustmddus@users.noreply.github.com>
2022-10-26 12:23:46 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,8 +1,3 @@
-/*
-n명의 사람이 영어 끝말잇기. 
-사람 수 n, 순서대로 말한 words. 
-가장 먼저 탈락하는 사람의 번호와 그 사람이 자신의 몇 번째에서 탈락하는지 구하라. 
-*/
 function solution(n, words) {
     words=words.map((i)=>[...i])
     let visited=[words[0].join('')]
@@ -13,7 +8,6 @@ function solution(n, words) {
             return [num,cnt]
         }
         visited.push(words[i+1].join(''))
-        console.log(visited)
     }
     return [0,0]
 }
