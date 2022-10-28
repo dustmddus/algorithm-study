a13b46068542709db7bfd07e40cc680ec145c1b8
Seung Yeon <82739503+dustmddus@users.noreply.github.com>
2022-10-29 01:14:55 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -3,22 +3,11 @@ n명 참가 게임. 1~n까지 배정.
 각 묶음에서 우승자가 1~n/2 배정
 */
 function solution(n,a,b)
-{
-    let x,y
-    if(a<b){
-        [x,y]=[a,b]
-    }
-    else{
-        [x,y]=[b,a]
-    }
-    
+{   
     let cnt=0
-    while(true){
-        if(x===y){
-            break
-        }
-        x=parseInt((x+1)/2)
-        y=parseInt((y+1)/2)
+    while(a!==b){
+        a=parseInt((a+1)/2)
+        b=parseInt((b+1)/2)
         cnt+=1
     }
     return cnt
