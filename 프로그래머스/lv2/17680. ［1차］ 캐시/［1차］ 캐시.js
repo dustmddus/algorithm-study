37af0a17abb223d5d17d7260a5207033944354b2
Seung Yeon <82739503+dustmddus@users.noreply.github.com>
2022-11-01 18:06:17 +0900

    [level 2] Title: [1차] 캐시, Time: 0.91 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,30 @@
+/*
+성능 측정 수행
+캐시 크기를 얼마로 해야 효율적인지 몰라 난감
+캐시 크기에 따른 실행시간 측정 프로그램
+*/
+function solution(cacheSize, cities) {
+    cities=cities.map((item)=>item.toLowerCase())
+    let visited=[]
+    let total=0
+    for(let item of cities){
+        if(cacheSize===0){
+            total+=5
+            continue
+        }
+        if(visited.includes(item)){
+            total++
+            visited=visited.filter((ele)=>ele!=item)
+            visited.push(item)
+        }else{
+            if(visited.length===cacheSize){
+                visited.shift()
+                visited.push(item)
+            }else{
+                visited.push(item)
+            }
+            total+=5
+        }
+    }
+    return total
+}
